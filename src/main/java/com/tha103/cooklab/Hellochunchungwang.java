095592daf7d23803da9936bc2e08f98c61bc4052
ChunChung Wang <dnlas2004@gmail.com>
2023-09-21 16:52:42 +0800

    更新了Hellochunchungwang

--- src/main/java/com/tha103/cooklab/Hellochunchungwang.java
+++ src/main/java/com/tha103/cooklab/Hellochunchungwang.java
@@ -1,9 +1,25 @@
 package com.tha103.cooklab;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Hellochunchungwang {
 
 	public static void main(String[] args) {
-		System.out.print("Hello ChunChungWang");
-	}
-
-}
+//		System.out.print("Hello ChunChungWang");
+	HelloDing Ding = new HelloDing();
+	HelloHo Ho =new  HelloHo();
+	HelloMartin  Martin  = new HelloMartin();
+	HelloTom Tom = new HelloTom();
+	HelloWorld_rs rs = new HelloWorld_rs();
+	List<String> data = new ArrayList<String>();
+	data.add(Ding.getClass().getName());
+	data.add(Ho.getClass().getName());
+	data.add(Martin.getClass().getName());
+	data.add(Tom.getClass().getName());
+	data.add(rs.getClass().getName());
+	int random = (int)Math.random()*5;
+	System.out.println("Hello "+data.get(random));
+	
+}}
