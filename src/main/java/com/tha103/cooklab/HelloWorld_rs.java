a13052bc2c84b38948097cd762a69e952cf0cfd5
Shin Ren <ruryu47@gmail.com>
2023-09-21 15:15:07 +0800

    測試新增HelloWorld_rs.java

---
+++ src/main/java/com/tha103/cooklab/HelloWorld_rs.java
@@ -0,0 +1,13 @@
+package com.tha103.cooklab;
+
+public class HelloWorld_rs {
+
+	private String brand;
+	private Integer price;
+	
+	public static void main(String[] args) {
+		System.out.println("qweradsfzxcv");
+
+	}
+
+}
