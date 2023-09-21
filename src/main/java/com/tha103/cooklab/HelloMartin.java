23f3463d2f11da57f3fa3f58351382dd699c6ffa
Martin Chang <martin80699@gmail.com>
2023-09-21 15:29:03 +0800

    2commit

--- src/main/java/com/tha103/cooklab/HelloMartin.java
+++ src/main/java/com/tha103/cooklab/HelloMartin.java
@@ -1,5 +1,11 @@
 package com.tha103.cooklab;
 
 public class HelloMartin {
-
+	private static String name ;
+	private static String age ;
+	
+	public static void main(String[] args) {
+		name = "Tom";
+		System.out.println("Hello"+name);
+	}
 }
