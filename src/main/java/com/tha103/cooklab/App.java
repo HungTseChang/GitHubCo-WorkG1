b95f8cb9b3b6b0b33f2a606d44f815bb7194c7bb
Martin Chang <martin80699@gmail.com>
2023-09-21 14:34:24 +0800

    First commit

---
+++ src/main/java/com/tha103/cooklab/App.java
@@ -0,0 +1,13 @@
+package com.tha103.cooklab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
